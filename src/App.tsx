6aebdb1e8316a0da1c45a41fce8ace6dbeeff963
Florjan96 <florianshiqerukaj@yahoo.com>
2022-12-20 17:37:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState(0)
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
