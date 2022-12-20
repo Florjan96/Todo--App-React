e46b1bc0a85e2d0a5c56f7a28e1979ba676f6eec
Florjan96 <florianshiqerukaj@yahoo.com>
2022-12-20 18:28:40 +0100

    Created background, state and a form to add a todo

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,28 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([
+    {id:1,title:"Read for one hour", completed:true},
+    {id:2,title:"Pick up groceries", completed:false},
+    {id:3,title:"Jog around park 3x", completed:true},
+    {id:4,title:"10 minutes meditation", completed:false}
+  ])
 
   return (
     <div className="App">
-      
+<div className="banner-card">
+  
+      <img src="./images/bg-desktop-dark.jpg" width="100%" />
+     
+      <div className="banner-text">
+<h1>T  O  D  O</h1>
+      </div>
+      <form>
+        <input type="text"  placeholder='Create a new todo . . .'/>
+        <button>Add</button>
+      </form>
+      </div>
+
     </div>
   )
 }
